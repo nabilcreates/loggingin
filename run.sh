d8cb12f6726566510b5063df09b5072916568d2a
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-02 17:14:14 +0800

    Fixed file system errors and also added config for the file

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "If you want this to automate, make sure to run this on every startup!"
-node ~/Documents/loginscript/script.js 
-echo "Please check the log file, if there is nothing, rerun the checklist @ checklist.txt"
+node ./script.js 
--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// TYPICAL REQUIRE STATEMENTS
+// REQUIRE STATEMENTS
 var fs = require('fs');
 let date = require('date-and-time');
 
-// DATA VARIABLE (TO WRITE)
+// Read config file
+let config = require('./config.json')
+
+// Used below to write to file
 var data = "Last Login: " + date.format(new Date() , 'DD/MM/YYYY HH:mm:ss') + "\n"
 
-// WRITES TO THE FILE AND THE WRITTEN DATA IS IN THE VARIABLE CALLED DATA (ABOVE)
-// Change the directory below to point to the log.txt file (depends on system)
-fs.writeFile("/home/nabil/Documents/loginscript/logging/log.txt",data, {flag: "a"},function(error,data){
+// Writes to file (if doesnt exist, it will create [because of the 'a' flag])
+fs.writeFile("./" + config.outputFileName + ".txt",data, {flag: "a"},function(error,data){
+
+	// Logs error if it exist
 	if(error){
 		console.log(error)
 	}
 
-	// UNCOMMENT BELOW TO GET FEEDBACK AFTER RUNNING SCRIPT (USED FOR DEBUGGING)
-	// console.log("Done")
+	// READ SILENT MODE CONFIG
+	if(!config.silentmode){
+		console.log("Done Writing!")
+	}
+	
 })
 
